1100402f831744cd50e01cdc74d469da236279e5
Dhanraj Singh <dhanraj@ssipmt.com>
2025-11-10 14:17:59 +0530

    Fix App.js import issue by removing incorrect Aboutus import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@
 import React, { useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
-import Aboutus from './components/Aboutus/Aboutus';
 import Footer from './components/Footer/Footer';
 import SignIn from './components/SignUp_In/SignIn';
 import SignUp from './components/SignUp_In/SignUp';
